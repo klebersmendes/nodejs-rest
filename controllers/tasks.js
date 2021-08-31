c0060c8622b5f3fd2e6edc883554aa5b4d0d287e
kleber.santos <kleber.santos@sanp.com.br>
2021-08-31 12:42:31 -0300

    api create, list, getItem, update and delete

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,10 +1,28 @@
 const Task = require('../models/tasks')
 
 module.exports = app => {
-    app.get('/tasks', (req, res) => res.send('Você está na rota de tarefas GET.'))
+    app.get('/tasks', (req, res) => {
+        Task.list(res)
+    })
+
+    app.get('/tasks/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Task.getItem(id, res)
+    })
+
     app.post('/tasks', (req, res) => {
         const task = req.body        
-        Task.create(task)
-        res.send('Você está na rota de tarefas POST.')
+        Task.create(task, res)        
+    })
+
+    app.patch('/tasks/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const values = req.body
+        Task.update(id, values, res)
+    })
+
+    app.delete('/tasks/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        Task.delete(id, res)
     })
 }
--- models/tasks.js
+++ models/tasks.js
@@ -1,20 +1,103 @@
 const moment = require('moment')
 const conexao = require('../infra/conect')
+class Task {    
+    create(task, res)
+    {
+        const create_at = moment().format('YYYY-MM-DD HH:MM:SS')
+        const update_at = moment().format('YYYY-MM-DD HH:MM:SS')
+        
+        const titleIsValid = task.title.length >= 5
+        const descriptionIsValid = task.description.length >= 10
+        const companyIsValid = task.company.length >= 5
+        
+        const validations = [
+            {
+                name: 'title',
+                valid: titleIsValid,
+                mensagem: 'Title is invalid, the text must be at least 5 characters.'
+            },
+            {
+                name: 'description',
+                valid: descriptionIsValid,
+                mensagem: 'Description is invalid, the text must be at least 10 characters.'
+            },
+            {
+                name: 'company',
+                valid: companyIsValid,
+                mensagem: 'Company is invalid.'
+            },
+        ]
 
-class Task {
-    create(task) {
-        const create_at = moment(task.create_at, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const update_at = moment().format('YYYY-MM-DD')        
-        const createAtTask = {...task, create_at, update_at}
-        const sql = 'INSERT INTO tasks SET ?'
-        conexao.query(sql, createAtTask, (erro, res) => {
+        const errors = validations.filter(field => !field.valid)
+
+        const isErrors = errors.length
+
+        if(isErrors){
+            res.status(400).json(errors)            
+        } else {
+            const createAtTask = {...task, create_at, update_at}
+            const sql = 'INSERT INTO tasks SET ?'
+
+            conexao.query(sql, createAtTask, (erro, response) => {
+                if(erro){
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(task)
+                }
+            })
+        }
+    }
+    
+    list(res)
+    {
+        const sql = 'SELECT * FROM tasks'
+
+        conexao.query(sql, (erro, response) => {
             if(erro){
-                console.log(erro)
+                res.status(400).json(erro)
             } else {
-                console.log(res)
+                res.status(200).json(response)
+            }
+        })
+    }
+
+    getItem(id, res)
+    {
+        const sql = `SELECT * FROM tasks WHERE id=${id}`
+        
+        conexao.query(sql, (erro, response) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
+            }
+        })
+    }
+
+    update(id, values, res)
+    {
+        const sql = `UPDATE tasks SET ? WHERE id=?`
+        
+        conexao.query(sql, [values, id], (erro, response) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
+            }
+        })
+    }
+
+    delete(id, res)
+    {
+        const sql = `DELETE FROM tasks WHERE id=?`
+        
+        conexao.query(sql, id, (erro, response) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
             }
         })
     }
 }
-
 module.exports = new Task
